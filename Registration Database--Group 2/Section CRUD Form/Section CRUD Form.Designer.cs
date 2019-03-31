4eeadaeee185627a0fdde752b9ff403233d76257
Christine Li <licf@umich.edu>
2019-03-31 19:15:46 -0400

    initial dependencies and references

---
+++ Registration Database--Group 2/Section CRUD Form/Section CRUD Form.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Section_CRUD_Form
+{
+	partial class SectionCRUDForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// SectionCRUDForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Name = "SectionCRUDForm";
+			this.Text = "Section";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
